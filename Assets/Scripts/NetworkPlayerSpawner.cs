65194f745f6e7a5da8fe5272dac306c3ba2221f4
James Allen <janone@hotmail.co.uk>
2025-11-24 19:42:08 +0000

    Player Spawner Reverted to Correct State

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -1,81 +1,38 @@
-using System.Collections.Generic;
-using FishNet.Connection;
 using FishNet.Managing;
+using FishNet.Connection;
 using FishNet.Transporting;
 using UnityEngine;
+using FishNet.Object;
 
-/// <summary>
-/// Spawns a player object for each connecting client.
-/// </summary>
 public sealed class NetworkPlayerSpawner : MonoBehaviour
 {
-    [SerializeField] private NetworkManager _networkManager;
-    [SerializeField] private FishNet.Object.NetworkObject _ownerPlayerPrefab;
-    [SerializeField] private FishNet.Object.NetworkObject _ghostPlayerPrefab;
-
-    private readonly HashSet<int> _spawnedConnectionIds = new HashSet<int>();
-    private bool _spawnedLocalPlayer;
-
-    private void Awake()
-    {
-        if (_networkManager == null)
-            _networkManager = FindObjectOfType<NetworkManager>();
-    }
+    [SerializeField] private NetworkManager networkManager;
+    [SerializeField] private FishNet.Object.NetworkObject playerPrefab;
 
     private void OnEnable()
     {
-        if (_networkManager != null)
-            _networkManager.ServerManager.OnRemoteConnectionState += HandleRemoteConnectionState;
+        if (networkManager == null)
+            networkManager = FindObjectOfType<NetworkManager>();
+
+        networkManager.ServerManager.OnRemoteConnectionState += HandleClientState;
     }
 
     private void OnDisable()
     {
-        if (_networkManager != null)
-            _networkManager.ServerManager.OnRemoteConnectionState -= HandleRemoteConnectionState;
+        networkManager.ServerManager.OnRemoteConnectionState -= HandleClientState;
     }
 
-    private void HandleRemoteConnectionState(NetworkConnection connection, RemoteConnectionStateArgs args)
+    private void HandleClientState(NetworkConnection conn, RemoteConnectionStateArgs args)
     {
-        if (args.ConnectionState == RemoteConnectionState.Started)
-            SpawnForConnection(connection);
-        else
-            _spawnedConnectionIds.Remove(connection.ClientId);
-    }
-
-    private void Update()
-    {
-        if (_spawnedLocalPlayer || _networkManager == null || !_networkManager.IsServer)
+        if (args.ConnectionState != RemoteConnectionState.Started)
             return;
 
-        NetworkConnection localConnection = _networkManager.ServerManager?.LocalConnection;
-        if (localConnection == null)
+        // SERVER ONLY
+        if (!networkManager.IsServer)
             return;
 
-        SpawnForConnection(localConnection);
-        _spawnedLocalPlayer = true;
-    }
-
-    private void SpawnForConnection(NetworkConnection connection)
-    {
-        if (connection == null || _spawnedConnectionIds.Contains(connection.ClientId))
-            return;
-
-        if (_ownerPlayerPrefab == null || _ghostPlayerPrefab == null)
-        {
-            Debug.LogWarning("Player prefabs are not assigned on NetworkPlayerSpawner.");
-            return;
-        }
-
-        FishNet.Object.NetworkObject ownerInstance = Instantiate(_ownerPlayerPrefab);
-        _networkManager.ServerManager.Spawn(ownerInstance.gameObject, connection);
-
-        FishNet.Object.NetworkObject ghostInstance = Instantiate(_ghostPlayerPrefab);
-        GhostMotor ghostMotor = ghostInstance.GetComponent<GhostMotor>();
-        if (ghostMotor != null)
-            ghostMotor.SetOwnerConnection(connection);
-
-        _networkManager.ServerManager.Spawn(ghostInstance.gameObject);
-
-        _spawnedConnectionIds.Add(connection.ClientId);
+        // Spawn actual networked player
+        NetworkObject player = Instantiate(playerPrefab);
+        networkManager.ServerManager.Spawn(player.gameObject, conn);
     }
 }
