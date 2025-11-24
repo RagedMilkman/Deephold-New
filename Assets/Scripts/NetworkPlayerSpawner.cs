eba1a584b3bfcd5da14181d64d5d47d5b4f17ff7
RagedMilkman <87068178+RagedMilkman@users.noreply.github.com>
2025-11-24 19:13:50 +0000

    Spawn local player ghost on host

--- Assets/Scripts/NetworkPlayerSpawner.cs
+++ Assets/Scripts/NetworkPlayerSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using FishNet.Connection;
 using FishNet.Managing;
 using FishNet.Transporting;
@@ -11,6 +12,9 @@ public sealed class NetworkPlayerSpawner : MonoBehaviour
     [SerializeField] private NetworkManager _networkManager;
     [SerializeField] private FishNet.Object.NetworkObject _ownerPlayerPrefab;
     [SerializeField] private FishNet.Object.NetworkObject _ghostPlayerPrefab;
+
+    private readonly HashSet<int> _spawnedConnectionIds = new HashSet<int>();
+    private bool _spawnedLocalPlayer;
 
     private void Awake()
     {
@@ -32,7 +36,28 @@ public sealed class NetworkPlayerSpawner : MonoBehaviour
 
     private void HandleRemoteConnectionState(NetworkConnection connection, RemoteConnectionStateArgs args)
     {
-        if (args.ConnectionState != RemoteConnectionState.Started)
+        if (args.ConnectionState == RemoteConnectionState.Started)
+            SpawnForConnection(connection);
+        else
+            _spawnedConnectionIds.Remove(connection.ClientId);
+    }
+
+    private void Update()
+    {
+        if (_spawnedLocalPlayer || _networkManager == null || !_networkManager.IsServer)
+            return;
+
+        NetworkConnection localConnection = _networkManager.ServerManager?.LocalConnection;
+        if (localConnection == null)
+            return;
+
+        SpawnForConnection(localConnection);
+        _spawnedLocalPlayer = true;
+    }
+
+    private void SpawnForConnection(NetworkConnection connection)
+    {
+        if (connection == null || _spawnedConnectionIds.Contains(connection.ClientId))
             return;
 
         if (_ownerPlayerPrefab == null || _ghostPlayerPrefab == null)
@@ -50,5 +75,7 @@ public sealed class NetworkPlayerSpawner : MonoBehaviour
             ghostMotor.SetOwnerConnection(connection);
 
         _networkManager.ServerManager.Spawn(ghostInstance.gameObject);
+
+        _spawnedConnectionIds.Add(connection.ClientId);
     }
 }
